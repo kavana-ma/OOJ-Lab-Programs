9092f973dc2d1a653dd1f6c3ceccf619fce0017d
Kavana M A <kavanama.cs23@bmsce.ac.in>
2024-10-21 14:22:13 +0530

    Create printArea.java

---
+++ printArea.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+abstract class Shape{
+double a,b,result;
+
+abstract void printArea();
+}
+
+class Rectangle extends Shape{
+void printArea(){
+System.out.println("Enter l and b of rectangle:");
+Scanner s=new Scanner(System.in);
+a=s.nextDouble();
+b=s.nextDouble();
+result=a*b;
+System.out.println(result+" sq units");
+}
+}
+class Triangle extends Shape{
+void printArea(){
+System.out.println("Enter b and h of triangle:");
+Scanner s=new Scanner(System.in);
+a=s.nextDouble();
+b=s.nextDouble();
+result=a*b/2;
+System.out.println(result+" sq units");
+}
+}
+class Circle extends Shape{
+void printArea(){
+System.out.println("Enter radius of circle:");
+Scanner s=new Scanner(System.in);
+a=s.nextDouble();
+result=3.142*a*a;
+System.out.println(result+" sq units");
+}
+}
+
+class printArea{
+public static void main(String args[]){
+Rectangle r=new Rectangle();
+Triangle t=new Triangle();
+Circle c=new Circle();
+r.printArea();
+t.printArea();
+c.printArea();
+}
+}
